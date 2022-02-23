9ea93317d993c4b0f63b6d5eafa3fa1a0cc60fb8
Yevhenii <freshjs.fte.@gmail.com>
2022-02-23 15:09:48 +0200

    Add innerHTML and textContent examples

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const body = document.body;
 
 body.title = 'Title';
 
-console.dir(body.children); // tags
-console.dir(body.childNodes); // tags + text
+const div2 = document.getElementById('div2');
 
-const div2 = document.getElementById('div3');
-// const div2 = document.querySelector('#div3');
-console.dir(div2);
+div2.innerHTML = '<div id="newdiv" >tesxt</div>';
 
-const divs = document.getElementsByClassName('class1');
-// const divs = document.querySelectorAll('.class1');
-console.dir(divs);
+const newdiv = document.querySelector('#newdiv');
 
-// getElementsByTagName
-// getElementsByName
+newdiv.textContent = 'New text';
+
+
+const div3 = document.getElementById('div3');
+
+const inners = div3.querySelectorAll('.mew')
+
+console.log(inners);
