a594ef0fec639167e2d25d2470461966d5e7a44c
Yevhenii <freshjs.fte.@gmail.com>
2022-02-25 16:26:28 +0200

    Add first task solution

---
+++ solution.js
@@ -0,0 +1,10 @@
+const link = document.getElementById('w3r')
+// const link = document.querySelector('#w3r')
+
+if (link !== null) {
+  console.log(link.type);
+  console.log(link.rel);
+  console.log(link.target);
+  console.log(link.hreflang);
+  console.log(link.href);
+}
